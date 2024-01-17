9635ea79d457f0c451f961a524b7b67d661c4f18
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2024-01-17 00:05:42 +0000

    Update and rename HealthSystem.cpp to HealthComponent.cpp

---
+++ HealthComponent.cpp
@@ -0,0 +1,49 @@
+// ©2024 JDSherbert. All Rights Reserved.
+
+#include "HealthComponent.h"
+
+/* ---------------------------- Method Definitions ------------------------------- */
+ 
+UHealthComponent::UHealthComponent(float InitialHealth, float InitialMaxHealth)
+    : Health(InitialHealth)
+    , MaxHealth(InitialMaxHealth)
+{
+    PrimaryComponentTick.bCanEverTick = false;
+
+    // Validate values and ensure they stay within expected ranges.
+    Clamp_MaxHealth(MaxHealth);
+    Clamp_CurrentHealth(CurrentHealth);
+    DeathCheck();
+}
+
+/* ------------------------------------------------------------------------------- */
+
+void UHealthComponent::InflictDamage(float Amount)
+{
+    float TempHealth = CurrentHealth;
+    CurrentHealth = Clamp_CurrentHealth(CurrentHealth -= Amount);
+
+    // Health changed?
+    if(TempHealth != CurrentHealth)
+    {
+        Event_OnHealthChanged(CurrentHealth);
+        DeathCheck();
+    }
+}
+
+/* ------------------------------------------------------------------------------- */
+
+void UHealthComponent::Heal(float Amount)
+{
+    float TempHealth = CurrentHealth;
+    CurrentHealth = Clamp_CurrentHealth(CurrentHealth += Amount);
+
+    // Health changed?
+    if(TempHealth != CurrentHealth)
+    {
+        Event_OnHealthChanged(CurrentHealth);
+        DeathCheck();
+    }
+}
+
+/* ------------------------------------------------------------------------------- */
